6009bc0eb128202bc734b58ea75d828e675a0feb
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 21:59:39 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -137,6 +137,69 @@ void Heap<T,PComparator>::pop()
 }
 
 
+/**
+ * @brief Construct a new Heap object
+ * 
+ * @param m ary-ness of heap tree (default to 2)
+ * @param c binary predicate function/functor that takes two items
+ *          as an argument and returns a bool if the first argument has
+ *          priority over the second.
+ */
+template <typename T, typename PComparator = std::less<T> >
+Heap<T, PComparator>::Heap(int m=2, PComparator c = PComparator()) {
+  s = 0;
+  ary = m;
+  comp = c;
+}
+
+/**
+* @brief Destroy the Heap object
+* 
+*/
+template <typename T, typename PComparator = std::less<T> >
+Heap<T, PComparator>::~Heap() {}
+
+/**
+ * @brief Push an item to the heap
+ * 
+ * @param item item to heap
+ */
+template <typename T, typename PComparator = std::less<T> >
+Heap<T, PComparator>::void push(const T& item) {
+  stuff.push_back(item);
+  T temp;
+  for (int i = stuff.size(); i > 1; i /= ary) {
+    if (comp(stuff[i - 1], stuff[(i / ary) - 1])) {
+      temp = stuff[i - 1];
+      stuff[i - 1] = stuff[(i / ary) - 1];
+      stuff[(i / ary) - 1] = temp;
+    } else {
+      break;
+    }
+  }
+}
+
+
+
+/// returns true if the heap is empty
+
+/**
+ * @brief Returns true if the heap is empty
+ * 
+ */
+template <typename T, typename PComparator = std::less<T> >
+bool Heap<T, PComparator>::empty() const {
+  return (stuff.empty());
+}
+
+  /**
+ * @brief Returns size of the heap
+ * 
+ */
+template <typename T, typename PComparator = std::less<T> >
+size_t Heap<T, PComparator>::size() const {
+  return stuff.size();
+}
 
 #endif
 
