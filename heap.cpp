3444d6fd75e4eb657c4d44232cd7f1b539e694c7
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 20:23:58 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -39,51 +39,7 @@ Heap::void push(const T& item) {
   }
 }
 
-/**
- * @brief Returns the top (priority) item
- * 
- * @return T const& top priority item
- * @throw std::underflow_error if the heap is empty
- */
-T const & Heap::top() const {
-  if (empty()) {
-    throw std::underflow_error;
-  } else {
-    return stuff[0];
-  }
-}
 
-/**
- * @brief Remove the top priority item
- * 
- * @throw std::underflow_error if the heap is empty
- */
-void Heap::pop() {
-  T temp = stuff[0];
-  stuff[0] = stuff[stuff.size() - 1];
-  stuff[stuff.size() - 1] = temp;
-  stuff.pop_back();
-  int i = 1;
-  bool done = true;
-  while (i < stuff.size() - 1) {
-    for (int j = 0; j < ary; j++) {
-      if ((i * ary) + j >= stuff.size()) {
-        break;
-      }
-      if (comp(stuff[i - 1], stuff[(i * ary) + j - 1])) {
-        temp = stuff[i - 1];
-        stuff[i - 1] = stuff[(i * ary) + j - 1];
-        stuff[(i * ary) + j - 1] = temp;
-        i = (i * ary) + j;
-        done = false;
-        break;
-      }
-    }
-    if (done) {
-      break;
-    }
-  }
-}
 
 /// returns true if the heap is empty
 
