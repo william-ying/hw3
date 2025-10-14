2104e988afcb9ed131b1bc6239bffe83fe500bb9
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 19:49:58 -0700

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,53 @@
+#include "heap.h"
+
+/**
+   * @brief Construct a new Heap object
+   * 
+   * @param m ary-ness of heap tree (default to 2)
+   * @param c binary predicate function/functor that takes two items
+   *          as an argument and returns a bool if the first argument has
+   *          priority over the second.
+   */
+  Heap(int m=2, PComparator c = PComparator());
+
+  /**
+  * @brief Destroy the Heap object
+  * 
+  */
+  ~Heap();
+
+  /**
+   * @brief Push an item to the heap
+   * 
+   * @param item item to heap
+   */
+  void push(const T& item);
+
+  /**
+   * @brief Returns the top (priority) item
+   * 
+   * @return T const& top priority item
+   * @throw std::underflow_error if the heap is empty
+   */
+  T const & top() const;
+
+  /**
+   * @brief Remove the top priority item
+   * 
+   * @throw std::underflow_error if the heap is empty
+   */
+  void pop();
+
+  /// returns true if the heap is empty
+
+  /**
+   * @brief Returns true if the heap is empty
+   * 
+   */
+  bool empty() const;
+
+    /**
+   * @brief Returns size of the heap
+   * 
+   */
+  size_t size() const;
