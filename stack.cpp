2fb8c1a459d95c375baba94087c50f0f15a3ed4d
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 21:30:33 -0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,27 +1,27 @@
 #include "stack.h"
 
-Stack<T>::Stack() {
+Stack::Stack() {
 
 }
 
-Stack<T>::~Stack();
-bool Stack<T>::empty() const {
+Stack::~Stack();
+bool Stack::empty() const {
     return (Stack.size() == 0);
 }
-size_t Stack<T>::size() const {
+size_t Stack::size() const {
     return (vector<T>::size());
 }
-void Stack<T>::push(const T& item) {
+void Stack::push(const T& item) {
     vector<T>::push_back(item);
 }
-void Stack<T>::pop() {
+void Stack::pop() {
     if (empty()) {
         throw std::underflow_error;
     } else {
         vector<T>::pop_back();
     }
 }
-const T& Stack<T>::top() const {
+const T& Stack::top() const {
     if (empty()) {
         throw std::underflow_error;
     } else {
