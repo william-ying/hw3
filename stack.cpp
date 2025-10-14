22a54004ee91817d983a280e2b0febdb658613b6
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 19:26:06 -0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,9 @@
 #include "stack.h"
 
+    Stack::Stack();
+    Stack::~Stack();
+    bool Stack::empty() const;
+    size_t Stack::size() const;
+    void Stack::push(const T& item);
+    void Stack::pop();  // throws std::underflow_error if empty
+    const T& Stack::top() const;
