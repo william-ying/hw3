1a40cfecaadcda82d28bed39d2a92e4f8994d9e9
William Wang <61264396+william-ying@users.noreply.github.com>
2025-10-13 22:27:44 -0700

    Update llrec.h

--- llrec.h
+++ llrec.h
@@ -84,7 +84,7 @@ Node* llfilter(Node* head, Comp pred)
     // Provide your implementation below
     //*********************************************
     if (head != nullptr) {
-        if (pred(head->val)) {
+        if (!pred(head->val)) {
             Node *temp = head->next;
             delete head;
             return llfilter(temp, pred);
